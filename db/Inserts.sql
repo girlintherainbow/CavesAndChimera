f8a48270e713fe51aee2e73c50ec136515d88cf1
glowo <gloworm93@gmail.com>
2018-03-16 13:26:05 -0500

    Insert Backup

---
+++ db/Inserts.sql
@@ -0,0 +1,261 @@
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (1, 'Acolyte', 'Insight, Religion, Language x2');
+
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (2, 'Charlatan', 'Deception, Sleight of Hand, Disguise Kit, Forgery Kit, PHB pg 128');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (3, 'Criminal', 'Deception, Stealth, Gaming Set, Thieve''s Tools, PHB pg129');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (4, 'Entertainer', 'Acrobatics, Performance, Disguise Kit, Musical Instrument, PHB pg 130');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (5, 'Folk Hero', 'Animal Handling, Survival, Artisan Tool Kit, Vehicle(land)');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (6, 'Guild Artisan', 'Insight, Persuasion, Artisan Tool Kit, Languages x1, PHB pg132');
+
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (7, 'Hermit', 'Medicine, Religion, Herbalism Kit, Languages x1, PHB pg134');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (8, 'Noble', 'History, Persuasion, Gaming Set, Languages x1, PHB pg135');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (9, 'Outlander', 'Athletics, Survival, Musical Instrument, Language x1, PHB pg136');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (10, 'Sage', 'Arcana, History, Languages x2, PHB pg137');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (11, 'Sailor', 'Athletics, Perception, Navigator''s Tools, Vehicle(water), PHB pg139');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (12, 'Soldier', 'Athletics, Intimidation, Gaming Set, Vehicle(land), PHB pg140');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (13, 'Urchin', 'Sleight of Hand, Stealth, Disguise Kit, Thieve''s Tools, PHB pg 141');
+
+
+
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (1, 'Acolyte', 'Insight, Religion, Language x2');
+
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (2, 'Charlatan', 'Deception, Sleight of Hand, Disguise Kit, Forgery Kit, PHB pg 128');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (3, 'Criminal', 'Deception, Stealth, Gaming Set, Thieve''s Tools, PHB pg129');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (4, 'Entertainer', 'Acrobatics, Performance, Disguise Kit, Musical Instrument, PHB pg 130');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (5, 'Folk Hero', 'Animal Handling, Survival, Artisan Tool Kit, Vehicle(land)');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (6, 'Guild Artisan', 'Insight, Persuasion, Artisan Tool Kit, Languages x1, PHB pg132');
+
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (7, 'Hermit', 'Medicine, Religion, Herbalism Kit, Languages x1, PHB pg134');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (8, 'Noble', 'History, Persuasion, Gaming Set, Languages x1, PHB pg135');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (9, 'Outlander', 'Athletics, Survival, Musical Instrument, Language x1, PHB pg136');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (10, 'Sage', 'Arcana, History, Languages x2, PHB pg137');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (11, 'Sailor', 'Athletics, Perception, Navigator''s Tools, Vehicle(water), PHB pg139');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (12, 'Soldier', 'Athletics, Intimidation, Gaming Set, Vehicle(land), PHB pg140');
+
+insert into characterbackground (characterbackgroundid, characterbackgroundname, backgroundproficiencies)
+values (13, 'Urchin', 'Sleight of Hand, Stealth, Disguise Kit, Thieve''s Tools, PHB pg 141');
+
+
+
+INSERT INTO equipment (equipmentid,equipmentcost, equipmentdamage, equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+VALUES (1 ,'Club', '1 sp', '1d4 bludgeoning', 2, 'Light',1,0)
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage, equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+VALUES (2,'Dagger', '2 gp', '1d4 piercing', 1 lb, 'Finess, light, thrown (range 20/60)',1,0)
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage, equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+VALUES (3, 'Greatclub', '2 sp', '1d8 bludgeoning', 10, 'Two-handed',1,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage, equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+VALUES (4, 'Handaxe', '5 gp', '1d6 slashing',2, 'Light, thrown (range 20/60)',1,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage, equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+VALUES (5, 'Javelin', '5 gp', '1d6 piercing', 2, 'Thrown (range 30/120)', 1, 0)
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage, equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+VALUES (6, 'Light Hammer', '2 gp', '1d4 bludgeoning', 2, 'Light, thrown (range 20/60), 1, 0)
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(7, 'Mace', '5 gp', '1d6 bludgeoning', 4, '---', 1, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(8, 'Quarterstaff', ' 2 sp', '1d6 bludgeoning', 4, 'Versatile (1d8)', 1, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(9, 'Sickle', '1 gp', '1d4 slashing', 2, 'Light',1,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(10, 'Spear', '1 gp', '1d6 piercing', 3, 'Thrown (range 20/60), versatile (1d8)', 1, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(11, 'Crossbow, light', '25 gp', '1d8 piercing', 5, 'Ammunition(range 80/320), loading, two-handed', 2,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(12, 'Dart', '5cp', '1d4 piercing', 0.25, 'Finesse, thrown(range 20/60)', 2,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(13, 'Shortbow', '25 gp', '1d6', 2, 'Ammunition (range 80/320), two-handed', 2, 0)
+
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(14, 'Sling', ' 1sp', '1d4 bludgeoning', 0, 'Ammunition (range 30/120)', 2, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(15, 'Battleaxe', '10 gp', '1d8 slashing', 4, 'Versatile (1d10)', 3, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(16, 'Flail', '10 gp', '1d8 bludgeoning', 2, '---', 3, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(17, 'Glaive', '20 gp', '1d10 slashing', 6, 'Heavy, reach, two-handed', 3, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(18, 'Great Axe', '30 gp', '1d12 slashing', 7, 'Heavy, two-handed', 3, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(19, 'Greatsword', '50 gp', '2d6 slashing', 6, 'Heavy, two-handed', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(20, 'Halberd', '20gp', '1d10 slashing', 6, 'Heavy, reach, two-handed', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(21, 'Lance', '10 gp', '1d12 piercing', 6, 'Reach, special', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(22, 'Longsword', '15 gp', '1d8 slashing', 3, 'Versatile(1d10)', 3, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(23, 'Maul', '10 gp', '2d6 bludgeoning' , 10, 'Heavy, two-handed', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(24, 'Morningstar', '15 gp', '1d8 Piercing', 4, '---', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(25, 'Pike', '5 gp', '1d10 piercing', 18, 'Heavy, Reach, two-handed', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(26, 'Rapier', '25 gp', '1d8 piercing', 2, 'Finesse', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(27, 'Scimitar', '25 gp', '1d6 slashing', 3, 'Finesse, light', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(28, 'Shortsword', '10 gp', '1d6 piercing', 2, 'Finesse, light', 3, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(29, 'Trident', '5 gp', '1d6 piercing', 4, 'Thrown (range 20/60), versatile(1d8), 3, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(30, 'War Pick', '5gp', ' 1d8 piercing', 2, '---', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(31, 'Warhammer', '15 gp', '1d8 bludgeoning', 2, 'Versatile(1d10)', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(32, 'Whip', '2 gp', '1d4 slashing', 3, 'Finesse, reach', 3,0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(33, 'Blowgun', '10 gp', '1 piercing', 1, 'Ammunition (range 25/100), loading', 4, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(34, 'Crossbow, hand', '75 gp', '1d6 piercing', 3, 'Ammunition (range (30/120), light, loading', 4, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(35, 'Crossbow, heavy', ' 50 gp','1d10 piercing', 18, 'Ammunition (range 100, 400), heavy, loading, two-handed', 4, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(36, 'Longbow', '50 gp', '1d8 piercing', 2, 'Ammunition (range 150/600), heavy, two-handed', 4, 0)
+
+
+INSERT INTO equipment (equipmentid, equipmentname, equipmentcost, equipmentdamage,
+			equipmentweight, equipmentproperties, equipmentclassid, equipmentlocationid)
+		VALUES(37, 'Net', '1 gp', '---', 3, 'Special, thrown (range 5/15)', 4,0)
